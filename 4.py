bf6182ec8e63b57732713467b81366e242f08a35
jin Hussein <jin.hussein96@gmail.com>
2021-03-24 19:21:30 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 14:29:21 2021
+
+@author: JIN_HUSSEIN
+"""
+'''
+# Location based replacement
+df.loc[2,'ST_NUM'] = 125
+'''
+import pandas as pd 
+data = pd.read_csv("country_vaccination_stats.csv")
+data = data[["country", "date", "daily_vaccinations", "vaccines"]]
+countries = data['country']
+a = countries[0]
+index = 0
+baslangic = 1
+for i in countries:
+    if (i == a ):
+        index = index +1
+    else:
+        a = i
+        if(not ( data[baslangic-1:index]['daily_vaccinations'].isnull().all())):
+            minvalue =  data[baslangic-1:index]['daily_vaccinations'].min()
+            data[baslangic-1:index]['daily_vaccinations'].fillna(minvalue, inplace = True)
+        else:
+            data[baslangic-1:index]['daily_vaccinations'].fillna(0)
+        baslangic = index+1
+        index = index +1
+        
+        
+if(not ( data[baslangic-1:index]['daily_vaccinations'].isnull().all())):
+    minvalue =  data[baslangic-1:index]['daily_vaccinations'].min()
+    data[baslangic-1:index]['daily_vaccinations'].fillna(minvalue, inplace = True)
+else:
+    data[baslangic-1:index]['daily_vaccinations'].fillna(0)
+data.to_csv('country_vaccination_stats1.csv')
+'''
+dfObj = pd.DataFrame(students, columns = ['Name' , 'Age', 'Citstajy'], index=['a', 'b', 'c'])
+'''
